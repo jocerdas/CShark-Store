2426a3bfa431612e5f15efb3402efc4f49ebfa65
Johan Cerdas <johan1950@live.com>
2026-08-01 22:04:08 -0600

    Creación de Pantalla Mantenimiento de Artículos y la Pantalla de la Tienda. En la CapaUI

---
+++ CapaUI/MantenimientoArticulos.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace CapaUI
+{
+    partial class MantenimientoArticulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitulo = new Label();
+            lblCodigo = new Label();
+            lblNombre = new Label();
+            lblPrecio = new Label();
+            lblStock = new Label();
+            lblEstado = new Label();
+            txtCodigo = new TextBox();
+            txtNombre = new TextBox();
+            txtPrecio = new TextBox();
+            txtStock = new TextBox();
+            chkActivo = new CheckBox();
+            dgvArticulos = new DataGridView();
+            btnSeleccionar = new Button();
+            btnCrear = new Button();
+            btnModificar = new Button();
+            btnVolver = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvArticulos).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTitulo.Location = new Point(251, 9);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(270, 22);
+            lblTitulo.TabIndex = 0;
+            lblTitulo.Text = "Mantenimiento de Artículos";
+            // 
+            // lblCodigo
+            // 
+            lblCodigo.AutoSize = true;
+            lblCodigo.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCodigo.Location = new Point(9, 47);
+            lblCodigo.Name = "lblCodigo";
+            lblCodigo.Size = new Size(70, 22);
+            lblCodigo.TabIndex = 1;
+            lblCodigo.Text = "Código";
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNombre.Location = new Point(148, 47);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(70, 22);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Nombre";
+            // 
+            // lblPrecio
+            // 
+            lblPrecio.AutoSize = true;
+            lblPrecio.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPrecio.Location = new Point(432, 47);
+            lblPrecio.Name = "lblPrecio";
+            lblPrecio.Size = new Size(70, 22);
+            lblPrecio.TabIndex = 3;
+            lblPrecio.Text = "Precio";
+            // 
+            // lblStock
+            // 
+            lblStock.AutoSize = true;
+            lblStock.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblStock.Location = new Point(571, 47);
+            lblStock.Name = "lblStock";
+            lblStock.Size = new Size(60, 22);
+            lblStock.TabIndex = 4;
+            lblStock.Text = "Stock";
+            // 
+            // lblEstado
+            // 
+            lblEstado.AutoSize = true;
+            lblEstado.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblEstado.Location = new Point(674, 47);
+            lblEstado.Name = "lblEstado";
+            lblEstado.Size = new Size(70, 22);
+            lblEstado.TabIndex = 5;
+            lblEstado.Text = "Estado";
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtCodigo.Location = new Point(9, 72);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(133, 30);
+            txtCodigo.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtNombre.Location = new Point(148, 72);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(278, 30);
+            txtNombre.TabIndex = 7;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPrecio.Location = new Point(432, 72);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(133, 30);
+            txtPrecio.TabIndex = 8;
+            // 
+            // txtStock
+            // 
+            txtStock.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtStock.Location = new Point(571, 72);
+            txtStock.Name = "txtStock";
+            txtStock.Size = new Size(93, 30);
+            txtStock.TabIndex = 9;
+            // 
+            // chkActivo
+            // 
+            chkActivo.AutoSize = true;
+            chkActivo.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            chkActivo.Location = new Point(674, 76);
+            chkActivo.Name = "chkActivo";
+            chkActivo.Size = new Size(89, 26);
+            chkActivo.TabIndex = 10;
+            chkActivo.Text = "Activo";
+            chkActivo.UseVisualStyleBackColor = true;
+            // 
+            // dgvArticulos
+            // 
+            dgvArticulos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvArticulos.Location = new Point(12, 118);
+            dgvArticulos.Name = "dgvArticulos";
+            dgvArticulos.Size = new Size(781, 272);
+            dgvArticulos.TabIndex = 11;
+            // 
+            // btnSeleccionar
+            // 
+            btnSeleccionar.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSeleccionar.Location = new Point(12, 403);
+            btnSeleccionar.Name = "btnSeleccionar";
+            btnSeleccionar.Size = new Size(164, 35);
+            btnSeleccionar.TabIndex = 12;
+            btnSeleccionar.Text = "&Seleccionar";
+            btnSeleccionar.UseVisualStyleBackColor = true;
+            // 
+            // btnCrear
+            // 
+            btnCrear.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCrear.Location = new Point(209, 403);
+            btnCrear.Name = "btnCrear";
+            btnCrear.Size = new Size(164, 35);
+            btnCrear.TabIndex = 13;
+            btnCrear.Text = "&Crear";
+            btnCrear.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            btnModificar.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnModificar.Location = new Point(421, 403);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(164, 35);
+            btnModificar.TabIndex = 14;
+            btnModificar.Text = "&Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnVolver
+            // 
+            btnVolver.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnVolver.Location = new Point(624, 403);
+            btnVolver.Name = "btnVolver";
+            btnVolver.Size = new Size(164, 35);
+            btnVolver.TabIndex = 15;
+            btnVolver.Text = "&Volver";
+            btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // MantenimientoArticulos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            ControlBox = false;
+            Controls.Add(btnVolver);
+            Controls.Add(btnModificar);
+            Controls.Add(btnCrear);
+            Controls.Add(btnSeleccionar);
+            Controls.Add(dgvArticulos);
+            Controls.Add(chkActivo);
+            Controls.Add(txtStock);
+            Controls.Add(txtPrecio);
+            Controls.Add(txtNombre);
+            Controls.Add(txtCodigo);
+            Controls.Add(lblEstado);
+            Controls.Add(lblStock);
+            Controls.Add(lblPrecio);
+            Controls.Add(lblNombre);
+            Controls.Add(lblCodigo);
+            Controls.Add(lblTitulo);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "MantenimientoArticulos";
+            Text = "Mantenimiento De Articulos";
+            ((System.ComponentModel.ISupportInitialize)dgvArticulos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitulo;
+        private Label lblCodigo;
+        private Label lblNombre;
+        private Label lblPrecio;
+        private Label lblStock;
+        private Label lblEstado;
+        private TextBox txtCodigo;
+        private TextBox txtNombre;
+        private TextBox txtPrecio;
+        private TextBox txtStock;
+        private CheckBox chkActivo;
+        private DataGridView dgvArticulos;
+        private Button btnSeleccionar;
+        private Button btnCrear;
+        private Button btnModificar;
+        private Button btnVolver;
+    }
+}
---
+++ CapaUI/Tienda.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace CapaUI
+{
+    partial class Tienda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitulo = new Label();
+            cbxNombreArticulo = new ComboBox();
+            lblCodigo = new Label();
+            lblNombre = new Label();
+            lblCantidad = new Label();
+            lblPrecioUnitario = new Label();
+            lblPrecioTotal = new Label();
+            lblTotal = new Label();
+            dataGridView1 = new DataGridView();
+            gbxMetodoPago = new GroupBox();
+            rbEfectivo = new RadioButton();
+            rbTarjeta = new RadioButton();
+            rbTransferencia = new RadioButton();
+            txtCodigo = new TextBox();
+            txtCantidad = new TextBox();
+            txtPrecioUnitario = new TextBox();
+            txtPrecioTotal = new TextBox();
+            txtTotalFacturar = new TextBox();
+            btnAgregar = new Button();
+            btnEliminar = new Button();
+            btnFacturar = new Button();
+            btnVolver = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            gbxMetodoPago.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTitulo.Location = new Point(306, 9);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(120, 22);
+            lblTitulo.TabIndex = 0;
+            lblTitulo.Text = "Facturación";
+            // 
+            // cbxNombreArticulo
+            // 
+            cbxNombreArticulo.Font = new Font("Consolas", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            cbxNombreArticulo.FormattingEnabled = true;
+            cbxNombreArticulo.Location = new Point(151, 60);
+            cbxNombreArticulo.Name = "cbxNombreArticulo";
+            cbxNombreArticulo.Size = new Size(419, 30);
+            cbxNombreArticulo.TabIndex = 1;
+            // 
+            // lblCodigo
+            // 
+            lblCodigo.AutoSize = true;
+            lblCodigo.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCodigo.Location = new Point(12, 35);
+            lblCodigo.Name = "lblCodigo";
+            lblCodigo.Size = new Size(70, 22);
+            lblCodigo.TabIndex = 2;
+            lblCodigo.Text = "Código";
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNombre.Location = new Point(151, 35);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(200, 22);
+            lblNombre.TabIndex = 3;
+            lblNombre.Text = "Nombre Del Artículo";
+            // 
+            // lblCantidad
+            // 
+            lblCantidad.AutoSize = true;
+            lblCantidad.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCantidad.Location = new Point(12, 93);
+            lblCantidad.Name = "lblCantidad";
+            lblCantidad.Size = new Size(90, 22);
+            lblCantidad.TabIndex = 4;
+            lblCantidad.Text = "Cantidad";
+            // 
+            // lblPrecioUnitario
+            // 
+            lblPrecioUnitario.AutoSize = true;
+            lblPrecioUnitario.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPrecioUnitario.Location = new Point(151, 93);
+            lblPrecioUnitario.Name = "lblPrecioUnitario";
+            lblPrecioUnitario.Size = new Size(160, 22);
+            lblPrecioUnitario.TabIndex = 5;
+            lblPrecioUnitario.Text = "Precio Unitario";
+            // 
+            // lblPrecioTotal
+            // 
+            lblPrecioTotal.AutoSize = true;
+            lblPrecioTotal.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPrecioTotal.Location = new Point(360, 93);
+            lblPrecioTotal.Name = "lblPrecioTotal";
+            lblPrecioTotal.Size = new Size(130, 22);
+            lblPrecioTotal.TabIndex = 6;
+            lblPrecioTotal.Text = "Precio Total";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTotal.Location = new Point(290, 376);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(170, 22);
+            lblTotal.TabIndex = 7;
+            lblTotal.Text = "Total a Facturar";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 156);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 219);
+            dataGridView1.TabIndex = 8;
+            // 
+            // gbxMetodoPago
+            // 
+            gbxMetodoPago.Controls.Add(rbTransferencia);
+            gbxMetodoPago.Controls.Add(rbTarjeta);
+            gbxMetodoPago.Controls.Add(rbEfectivo);
+            gbxMetodoPago.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            gbxMetodoPago.Location = new Point(588, 23);
+            gbxMetodoPago.Name = "gbxMetodoPago";
+            gbxMetodoPago.Size = new Size(200, 125);
+            gbxMetodoPago.TabIndex = 9;
+            gbxMetodoPago.TabStop = false;
+            gbxMetodoPago.Text = "Métodos de Pago";
+            // 
+            // rbEfectivo
+            // 
+            rbEfectivo.AutoSize = true;
+            rbEfectivo.Location = new Point(3, 26);
+            rbEfectivo.Name = "rbEfectivo";
+            rbEfectivo.Size = new Size(108, 26);
+            rbEfectivo.TabIndex = 0;
+            rbEfectivo.TabStop = true;
+            rbEfectivo.Text = "Efectivo";
+            rbEfectivo.UseVisualStyleBackColor = true;
+            // 
+            // rbTarjeta
+            // 
+            rbTarjeta.AutoSize = true;
+            rbTarjeta.Location = new Point(6, 58);
+            rbTarjeta.Name = "rbTarjeta";
+            rbTarjeta.Size = new Size(98, 26);
+            rbTarjeta.TabIndex = 1;
+            rbTarjeta.TabStop = true;
+            rbTarjeta.Text = "Tarjeta";
+            rbTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // rbTransferencia
+            // 
+            rbTransferencia.AutoSize = true;
+            rbTransferencia.Location = new Point(6, 90);
+            rbTransferencia.Name = "rbTransferencia";
+            rbTransferencia.Size = new Size(158, 26);
+            rbTransferencia.TabIndex = 2;
+            rbTransferencia.TabStop = true;
+            rbTransferencia.Text = "Transferencia";
+            rbTransferencia.UseVisualStyleBackColor = true;
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtCodigo.Location = new Point(12, 60);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(133, 30);
+            txtCodigo.TabIndex = 10;
+            // 
+            // txtCantidad
+            // 
+            txtCantidad.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtCantidad.Location = new Point(12, 120);
+            txtCantidad.Name = "txtCantidad";
+            txtCantidad.Size = new Size(133, 30);
+            txtCantidad.TabIndex = 11;
+            // 
+            // txtPrecioUnitario
+            // 
+            txtPrecioUnitario.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPrecioUnitario.Location = new Point(151, 118);
+            txtPrecioUnitario.Name = "txtPrecioUnitario";
+            txtPrecioUnitario.Size = new Size(203, 30);
+            txtPrecioUnitario.TabIndex = 12;
+            // 
+            // txtPrecioTotal
+            // 
+            txtPrecioTotal.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPrecioTotal.Location = new Point(360, 118);
+            txtPrecioTotal.Name = "txtPrecioTotal";
+            txtPrecioTotal.Size = new Size(203, 30);
+            txtPrecioTotal.TabIndex = 13;
+            // 
+            // txtTotalFacturar
+            // 
+            txtTotalFacturar.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtTotalFacturar.Location = new Point(290, 399);
+            txtTotalFacturar.Name = "txtTotalFacturar";
+            txtTotalFacturar.Size = new Size(203, 30);
+            txtTotalFacturar.TabIndex = 14;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAgregar.Location = new Point(12, 394);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(133, 35);
+            btnAgregar.TabIndex = 15;
+            btnAgregar.Text = "&Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEliminar.Location = new Point(151, 394);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(133, 35);
+            btnEliminar.TabIndex = 16;
+            btnEliminar.Text = "&Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnFacturar
+            // 
+            btnFacturar.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnFacturar.Location = new Point(499, 394);
+            btnFacturar.Name = "btnFacturar";
+            btnFacturar.Size = new Size(133, 35);
+            btnFacturar.TabIndex = 17;
+            btnFacturar.Text = "&Facturar";
+            btnFacturar.UseVisualStyleBackColor = true;
+            // 
+            // btnVolver
+            // 
+            btnVolver.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnVolver.Location = new Point(655, 394);
+            btnVolver.Name = "btnVolver";
+            btnVolver.Size = new Size(133, 35);
+            btnVolver.TabIndex = 18;
+            btnVolver.Text = "&Volver";
+            btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // Tienda
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            ControlBox = false;
+            Controls.Add(btnVolver);
+            Controls.Add(btnFacturar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtTotalFacturar);
+            Controls.Add(txtPrecioTotal);
+            Controls.Add(txtPrecioUnitario);
+            Controls.Add(txtCantidad);
+            Controls.Add(txtCodigo);
+            Controls.Add(gbxMetodoPago);
+            Controls.Add(dataGridView1);
+            Controls.Add(lblTotal);
+            Controls.Add(lblPrecioTotal);
+            Controls.Add(lblPrecioUnitario);
+            Controls.Add(lblCantidad);
+            Controls.Add(lblNombre);
+            Controls.Add(lblCodigo);
+            Controls.Add(cbxNombreArticulo);
+            Controls.Add(lblTitulo);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "Tienda";
+            Text = "Tienda";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            gbxMetodoPago.ResumeLayout(false);
+            gbxMetodoPago.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitulo;
+        private ComboBox cbxNombreArticulo;
+        private Label lblCodigo;
+        private Label lblNombre;
+        private Label lblCantidad;
+        private Label lblPrecioUnitario;
+        private Label lblPrecioTotal;
+        private Label lblTotal;
+        private DataGridView dataGridView1;
+        private GroupBox gbxMetodoPago;
+        private RadioButton rbTransferencia;
+        private RadioButton rbTarjeta;
+        private RadioButton rbEfectivo;
+        private TextBox txtCodigo;
+        private TextBox txtCantidad;
+        private TextBox txtPrecioUnitario;
+        private TextBox txtPrecioTotal;
+        private TextBox txtTotalFacturar;
+        private Button btnAgregar;
+        private Button btnEliminar;
+        private Button btnFacturar;
+        private Button btnVolver;
+    }
+}
